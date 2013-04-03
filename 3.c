80adc97449e0be6c784f669b2274229287bd5c2f
sj2903 <sj2903@mail.ru>
2013-04-03 17:14:16 +0400

    Added print of manual

--- 3.c
+++ 3.c
@@ -3,8 +3,10 @@
 #include <string.h>
 #include "mylib.h"
 
-int main(int argc, char **argv)
+int main(int argc, char **argv) 
 {
-    
+    if(print_manual(argc,argv)) {
+    	return 0;
+	}
     return 0;
 }

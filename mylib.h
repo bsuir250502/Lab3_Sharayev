7711027d31713c57c672d395d92576f22847353a
sj2903 <sj2903@mail.ru>
2013-04-03 17:08:23 +0400

    Added some prototypes

---
+++ mylib.h
@@ -0,0 +1,12 @@
+#ifndef _MYLIB_H_
+#define _MYLIB_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* myfgets(char *, int);
+void print_manual(int, char **);
+int input_number_in_range(int, int);
+
+#endif

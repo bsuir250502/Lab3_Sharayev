e73a3842d9cefead0ff7308e9f13a30722b24beb
sj2903 <sj2903@mail.ru>
2013-04-08 15:43:01 +0300

    Added print manual

--- 3.c
+++ 3.c
@@ -3,10 +3,25 @@
 #include <string.h>
 #include "mylib.h"
 
+char read_argument(int, char **);
+
+
 int main(int argc, char **argv) 
 {
-    if(print_manual(argc,argv)) {
-    	return 0;
-	}
+    if(read_argument(argc,argv)) {
+        print_manual();
+        return 0;
+    }
+    
     return 0;
+}
+
+char read_argument(int argc, char **argv) {
+    if(argc == 2) {
+        if(!(strcmp(argv[1], "-h")) ) {
+            return 'h';
+        } 
+    }
+
+    return '0';
 }

8bb7c1d97a06e738a71be244fbdf35cd47fd97cb
Eugen <sj2903@mail.ru>
2013-04-14 00:05:48 +0300

    added fit person and add person funct-s

--- 3.c
+++ 3.c
@@ -13,22 +13,27 @@ typedef struct {
 int calculate_distance(coordinate_t, coordinate_t);
 
 typedef struct person {
+	struct person *next;
     char surname[16];
     coordinate_t coord;
 } person_t;
 
 typedef struct hospital {
+	person_t *first;
+	person_t *last;
     int num_of_beds;
     int num_of_empty_beds;
     coordinate_t coord;
-    /*int *(distance)(coordinate_t, coordinate_t); */
+    int (*dist)(coordinate_t, coordinate_t); 
 } hospital_t;
 
 
 
 
-int read_information(hospital_t *);
+int read_information_of_hospitals(hospital_t *);
 char read_argument(int, char **);
+int add_patient(hospital_t *, int);
+int fit_person_to_the_hospital(hospital_t *,person_t *);
 /*
 
 int calculate_distance(coordinate_t, coordinate_t);
@@ -39,22 +44,19 @@ int main(int argc, char **argv)
 {
     int num_of_hospitals;
     hospital_t *hosp = NULL;
-    /*person_t *person;*/
 
     if (read_argument(argc, argv) == 'h') {
         print_manual();
         return 0;
     }
 
-    num_of_hospitals = read_information(hosp);
+    num_of_hospitals = read_information_of_hospitals(hosp);
     printf("num_of_hospitals = %d \n", num_of_hospitals);
-    /*hosp = read_information(&num_of_hospitals);
-       person = fitting_persons_to_the_hospitals(hosp, num_of_hospitals); */
 
     return 0;
 }
 
-int read_information(hospital_t * hosp)
+int read_information_of_hospitals(hospital_t * hosp)
 {
     int num_of_hospitals, i;
     char input_buffer[128], *endptr;
@@ -77,64 +79,51 @@ int read_information(hospital_t * hosp)
 		fgets(input_buffer,SIZE(input_buffer),fp);
 		hosp[i].coord.x = strtol(input_buffer, &endptr, 10);
 		hosp[i].coord.y = strtol(endptr, &endptr, 10);
+		hosp[i].dist = calculate_distance;
 	}
+	/*
 	for(i = 0; i < num_of_hospitals;i++) {
 	printf("(%d;%d)", hosp[i].coord.x ,hosp[i].coord.y );
 	}
+	*/
 
     return num_of_hospitals;
 }
 
-/*hospital_t* read_information(int *num_of_hospitals) {
-	char input_buffer[128], *endptr;
-	int i, num_of_beds[MAX_NUM_OF_HOSPITALS], num_of_empty_beds[MAX_NUM_OF_HOSPITALS];
-	FILE *fp = fopen("Input.txt", "r");
-	hospital_t *hosp;
 
-	for(i=0; (fgets(input_buffer,SIZE(input_buffer),fp));i++) {
-		num_of_beds[i]=strtol(input_buffer, &endptr, 4);
-		
-	}
-	*num_of_hospitals = i;
-	for(i=0; i < *num_of_hospitals; i++) {
-		fgets(input_buffer,SIZE(input_buffer),fp);
-		num_of_empty_beds[i]=strtol(input_buffer, &endptr, 4);
-	}
-	hosp=(hospital_t *)malloc(*num_of_hospitals * sizeof(hospital_t));
-	for(i=0; i < *num_of_hospitals; i++) {
-		hosp[i].num_of_beds = num_of_beds[i];
-		hosp[i].num_of_empty_beds = num_of_empty_beds[i];
-	}
+int add_patient(hospital_t *hosp, int num_of_hospitals) {
+	int i, better_hosp_num = 0;
+	person_t *patient;
+	patient = (person_t *) malloc(sizeof(patient));
+	patient->next = NULL;
 
-	return hosp;
-}
-
-
-person_t* fitting_persons_to_the_hospitals(const hospital_t *hosp, const int num_of_hospitals) {
-	int num_of_persons, i, j, hospital_num;
-	person_t *person;
-	num_of_persons=input_number_in_range(1,100);
-	person = (person_t *)malloc(num_of_persons * sizeof(person_t));
-	for(i = 0; i < num_of_persons; i++) {
-		printf("Supply surname of the %d person:\n", i+1);
-		myfgets(person[i].surname,16);
-		for(j = 0; j < num_of_hospitals; j++) {
-			if( hosp[j].num_of_beds > hosp[j].num_of_empty_beds) {
-				hospital_num=j;
+	printf("Specify the surname of patient(end, to exit):  ");
+	myfgets(patient->surname, 16);
+	printf("His coordinates(x,y): \n");
+	patient->coord.x=input_number_in_range(1,100);
+	patient->coord.y=input_number_in_range(1,100);
+	for(i=0; i < num_of_hospitals; i++) {
+		if( (hosp[i].num_of_beds > hosp[i].num_of_empty_beds) || 
+			(hosp[i].dist(hosp[i].coord,patient->coord) < hosp[better_hosp_num].dist(hosp[better_hosp_num].coord,patient->coord)) ) {
+				better_hosp_num = i;
 			}
-		}
-		person[i].hospital = hospital_num;
 	}
+	fit_person_to_the_hospital(hosp[better_hosp_num], patient);
 
-	return person;
+	return 0;
 }
 
-int calculate_distance(coordinate_t a, coordinate_t b ) {
-	int square_dist;
-	square_dist = pow(a.x-b.x, 2) + pow(a.y-b.y, 2);
-	return pow(square_dist, 1/2); 
+int fit_person_to_the_hospital(hospital_t *hospital,person_t *patient) {
+	if(!(hospital->first)) {
+		hospital->first = patient;
+	}
+	else {
+		hospital->last->next = patient;
+	}
+	hospital->last = patient;
+
+	return 0;
 }
-*/
 
 char read_argument(int argc, char **argv) {
     if (argc == 2) {
@@ -144,4 +133,10 @@ char read_argument(int argc, char **argv) {
     }
 
     return '0';
+}
+
+int calculate_distance(coordinate_t a, coordinate_t b ) {
+	int square_dist;
+	square_dist = (a.x-b.x)*(a.x-b.x) + (a.y-b.y)*(a.y-b.y);
+	return square_dist; 
 }
